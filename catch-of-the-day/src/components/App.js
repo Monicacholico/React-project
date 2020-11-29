4a08ecf00bfa632ac7a476d0aa3a734695f3413c
Monicacholico <monicachol@hotmail.com>
2020-11-29 01:14:34 -0600

    on half way

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFisehs from "../sample-fishes"
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = (fish) => {
+        console.log('Adding a fish!')
+        // 1. Take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({ fishes })
+    }
+    loadSampleFishes = () => {
+        alert('sample');
+        this.setState({fishes: sampleFisehs});
+    }
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +29,9 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory 
+                addFish={this.addFish} 
+                loadSampleFishes={this.loadSampleFishes}/>
             </div>
         )
     }
--- catch-of-the-day/src/components/Storepicker.js
+++ catch-of-the-day/src/components/Storepicker.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import {getFunName} from '../helpers';
-
+  
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    myInput = React.createRef();
+  myInput = React.createRef();
+  
 
-    goToStore(event) {
-        // 1. stop the from from submitting
-        event.preventDefault();
-        // 2. get the text from that input
-        console.log(this);
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
 
-        // 3. Change the page to /store/wahtever-they-enteredd
-    }
-    componentDidMount() {
-        console.log(this);
-    }
-    render() {
-        
-        return (
-         <form className="store-selector" onSubmit={this.componentDidMount}>
-            <h2>Please Enter a Store</h2>  
-            <input 
-            type="text" 
-            ref={this.myInput}
-            required 
-            placeholder="Store Name" 
-            defaultValue={getFunName()}/>
-            <button type="submit">Visit Store</button>  
-        </form>   
-        )
-    }
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
