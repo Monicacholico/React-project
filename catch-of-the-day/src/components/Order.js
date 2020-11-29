e2ea154f3783aff68aa15318f9ed4536ff222353
Monicacholico <monicachol@hotmail.com>
2020-11-28 18:24:02 -0600

    adding components

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Order extends React.Component {
+    render() {
+        return(
+            <div className="order">
+                This is the Order!!!
+            </div>
+        )
+    }
+}
+
+export default Order;
