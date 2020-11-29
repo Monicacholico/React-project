934f4d81d102c5555ed55acb98522572b8099ef9
Monicacholico <monicachol@hotmail.com>
2020-11-28 23:56:28 -0600

    commited after running codemods to update component life cycle

--- catch-of-the-day/src/components/Storepicker.js
+++ catch-of-the-day/src/components/Storepicker.js
@@ -1,16 +1,34 @@
-import React, {Fragment} from 'react'
+import React from 'react';
+import {getFunName} from '../helpers';
 
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore(event) {
+        // 1. stop the from from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        console.log(this);
+
+        // 3. Change the page to /store/wahtever-they-enteredd
+    }
+    componentDidMount() {
+        console.log(this);
+    }
     render() {
+        
         return (
-        <Fragment>
-         <form className="store-selector">
+         <form className="store-selector" onSubmit={this.componentDidMount}>
             <h2>Please Enter a Store</h2>  
-            <input type="text" required placeholder="Store Name"/>
+            <input 
+            type="text" 
+            ref={this.myInput}
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()}/>
             <button type="submit">Visit Store</button>  
-        </form>
-        </Fragment>    
+        </form>   
         )
     }
 }
