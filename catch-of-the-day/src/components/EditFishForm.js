b2c900b3a4ff56de8a02736a5fb1a13139f581c3
Monicacholico <monicachol@hotmail>
2020-12-04 22:28:44 -0600

    adding props to inventory

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -4,8 +4,17 @@ import React from 'react';
 class EditFishForm extends React.Component {
     render() {
         return (
-            <p>Edit Fish!</p>
-        )
+            <div clasName="fish-edit">
+                <input type="text" name="name"/>
+                <input type="text" name="price"/>
+                <select type="text" name="status">
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">sold out!</option>
+                </select>
+                <textarea name="desc"/>
+                <input type="text" name="image"/>
+            </div>
+        );
     }
 }
 
