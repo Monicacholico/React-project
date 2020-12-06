f409b17b4c53854b5045ff13e738296cbc9b1c53
Monicacholico <monicachol@hotmail.com>
2020-12-05 20:40:54 -0600

    adding edit to inventory

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
-
 class EditFishForm extends React.Component {
+    handleChange = event => {
+        console.log(event.currentTarget.value);
+    }
     render() {
         return (
-            <div clasName="fish-edit">
-                <input type="text" name="name"/>
-                <input type="text" name="price"/>
-                <select type="text" name="status">
-                    <option value="available">Fresh!</option>
-                    <option value="unavailable">sold out!</option>
+            <div className="fish-edit">
+                <input 
+                type="text" 
+                name="name" 
+                onChange={this.handleChange} 
+                value={this.props.name}/>
+                <input 
+                type="text" 
+                name="price" 
+                onChange={this.handleChange} 
+                value={this.props.price}/>
+                <select 
+                type="text" 
+                name="status" 
+                onChange={this.handleChange} 
+                value={this.props.status}>
+                    <option value="available" >Fresh!</option>
+                    <option value="unavailable" >sold out!</option>
                 </select>
-                <textarea name="desc"/>
-                <input type="text" name="image"/>
+                <textarea 
+                name="desc" 
+                onChange={this.handleChange} 
+                value={this.props.desc}/>
+                <input 
+                type="text" 
+                name="image" 
+                onChange={this.handleChange} 
+                value={this.props.image}/>
             </div>
         );
     }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,9 +7,11 @@ class Inventory extends React.Component {
         return (
             <div className="inventory">
                 <h2>Inventory!!!</h2>
-                {Object.keys(this.props.fishes).map(key => <EditFishForm fish={this.props.fishes[key]}/>)}
+                {Object.keys(this.props.fishes).map(key => 
+                <EditFishForm key={key} fish={this.props.fishes[key]}/>)}
                 <AddFishForm addFish={this.props.addFish}/>
-                <button onClick={this.props.loadSampleFishes}>Load sample Fishes</button>
+                <button onClick={this.props.loadSampleFishes}>
+                Load sample Fishes</button>
             </div>
         )
     }
