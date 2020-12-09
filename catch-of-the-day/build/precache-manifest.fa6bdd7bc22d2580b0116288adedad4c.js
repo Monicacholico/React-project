1a49453a5e0010aa61eaa0bd3af708d13ef9537d
Monicacholico <monicachol@hotmail>
2020-12-09 17:14:54 -0600

    deploying with vercel

---
+++ catch-of-the-day/build/precache-manifest.fa6bdd7bc22d2580b0116288adedad4c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec452f8600ac608a6349bcfd27b6fbcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ac954b48bd4a758ebdd",
+    "url": "/static/css/main.f18463bd.chunk.css"
+  },
+  {
+    "revision": "3313ff5ed7065f20ffc6",
+    "url": "/static/js/2.e977835c.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.e977835c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac954b48bd4a758ebdd",
+    "url": "/static/js/main.33cb7fa1.chunk.js"
+  },
+  {
+    "revision": "91973092a8adabe4af5a",
+    "url": "/static/js/runtime-main.76b6e058.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
